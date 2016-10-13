1b6bf482d6a0ae75cc1bc1b74c852c7706814af5
kahlveen <kahlveen321@gmail.com>
2016-10-13 21:41:00 +0800

    Floating grey box on the right

--- special_template.php
+++ special_template.php
@@ -13,19 +13,15 @@ if (have_posts()) :
 	while (have_posts()) : the_post(); ?>
 
 	<article class="post page">
-	<!-- div container -->
-		<div class="column-container clearfix">
-		
-			<div class="title-column">
-				<h2><?php the_title();?></h2>
-				<h3>The monkey!</h3>		
-			</div>
-
-			<div class="text-column">
-				<?php the_content();?>
-			</div>
-		
-		</div> <!-- column-container -->
+		<h2><?php the_title();?></h2>
+	
+		<div class="info-box">
+			<h4>This is a cool sidebox</h4>
+			<p>People completing such an event within the strict event time cutoffs are agreed to be recognized as "Ironmen"; the plural "Ironmans" refers to multiples of "Ironman" as a short form of "Ironman Triathlon." In the triathlon community an Ironman is someone who has completed a race of the appropriate distance, whether or not it falls under the aegis of WTC.</p>
+		</div>
+	
+		<?php the_content();?>	
+	
 	</article>
 	<?php endwhile;
 
